88bbea9bc3c4ec3323a705a9d55b0ead2dc87af0
jiadonglou <jl3937@cornell.edu>
2021-03-03 11:28:06 -0500

    Update world_clock.py

--- Lab 2/world_clock.py
+++ Lab 2/world_clock.py
@@ -87,6 +87,24 @@ disp = st7789.ST7789(
     y_offset=40,
 )
 
+def ScaleImage(image):
+# Scale the image to the smaller screen dimension
+	image_ratio = image.width / image.height
+	screen_ratio = width / height
+	if screen_ratio < image_ratio:
+	    scaled_width = image.width * height // image.height
+	    scaled_height = height
+	else:
+	    scaled_width = width
+	    scaled_height = image.height * width // image.width
+	image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+	# Crop and center the image
+	x = scaled_width // 2 - width // 2
+	y = scaled_height // 2 - height // 2
+	image = image.crop((x, y, x + width, y + height))
+	return image
+
 # Create blank image for drawing.
 # Make sure to create image with mode 'RGB' for full color.
 height = disp.width  # we swap height/width to rotate it to landscape!
@@ -164,6 +182,7 @@ while True:
 
     background = Image.open("8.jpeg")
     background = background.convert('RGB').resize((240, 135), Image.BICUBIC)
+    background = ScaleImage(background)
     draw = ImageDraw.Draw(background)
     disp.image(background)
     # Display image.
