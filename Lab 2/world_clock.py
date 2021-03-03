8f4467ab692cf50edfdce8001089304d20a3e76f
jiadonglou <jl3937@cornell.edu>
2021-03-03 11:48:28 -0500

    Update world_clock.py

--- Lab 2/world_clock.py
+++ Lab 2/world_clock.py
@@ -188,6 +188,6 @@ while True:
     draw.text((x+100, y+85), TIME, font=font, fill="#FFFFFF")
 
     # Display image.
-    disp.image(background)
+    disp.image(background,rotation)
     #disp.image(image, rotation)
     #time.sleep(1)
