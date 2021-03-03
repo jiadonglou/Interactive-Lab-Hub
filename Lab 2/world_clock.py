a14e1d59b58c3e94c72c073a00e4f41c6ebfd82e
jiadonglou <jl3937@cornell.edu>
2021-03-03 11:59:16 -0500

    Update world_clock.py

--- Lab 2/world_clock.py
+++ Lab 2/world_clock.py
@@ -170,7 +170,7 @@ while True:
     # Draw a black filled box to clear the image.
 
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
-	background = Image.open("8.jpeg")
+    background = Image.open("8.jpeg")
 	background = ScaleImage(background)
 	draw = ImageDraw.Draw(background)
 
