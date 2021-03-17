919959353474febf64279af9f2f01442343e590c
jiadonglou <jl3937@cornell.edu>
2021-03-17 19:51:02 -0400

    Update world_clock_speech.py

--- Lab 3/speech_world_clock/world_clock_speech.py
+++ Lab 3/speech_world_clock/world_clock_speech.py
@@ -107,7 +107,7 @@ def Speech2Text():
 
     model = Model("model")
     # You can also specify the possible word list
-    rec = KaldiRecognizer(model, wf.getframerate(), "east west day night shanghai tokyo")
+    rec = KaldiRecognizer(model, wf.getframerate(), "east west day night shanghai paris newyork")
 
     while True:
         data = wf.readframes(4000)
