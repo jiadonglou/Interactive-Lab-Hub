60b642db6fc076ca013369a1613541db7fa09b91
jiadonglou <jl3937@cornell.edu>
2021-03-24 15:43:27 -0400

    Update cap_test.py

--- Lab 4/cap_test.py
+++ Lab 4/cap_test.py
@@ -8,7 +8,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 mpr121 = adafruit_mpr121.MPR121(i2c)
 #mpr121.threshold(0,10)
-print(MPR121_Channel.threshold(mpr121[0]))
+print(adafruit_mpr121.threshold(mpr121[0]))
 while True:
 	for i in range(12):
 		if mpr121[i].value:
