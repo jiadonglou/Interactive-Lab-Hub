71eba552f9b467da6f5e1c0367a57023d541c537
jiadonglou <jl3937@cornell.edu>
2021-03-24 15:38:51 -0400

    Update cap_test.py

--- Lab 4/cap_test.py
+++ Lab 4/cap_test.py
@@ -7,10 +7,10 @@ import adafruit_mpr121
 i2c = busio.I2C(board.SCL, board.SDA)
 
 mpr121 = adafruit_mpr121.MPR121(i2c)
-mpr121.threshold(0,10)
+#mpr121.threshold(0,10)
+print(mpr121[0].value)
 while True:
 	for i in range(12):
 		if mpr121[i].value:
-			
 			print(f"Banana {i} touched!")
 	time.sleep(0.25)  # Small delay to keep from spamming output messages.
