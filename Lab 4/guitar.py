8607374c944ede8015cde2481e74694ce7f64031
jiadonglou <jl3937@cornell.edu>
2021-03-24 17:22:16 -0400

    Create guitar.py

---
+++ Lab 4/guitar.py
@@ -0,0 +1,100 @@
+import time
+import subprocess
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+
+
+s1 = [0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0]
+s2 = [0,0,0,0,4,0,4,0,0,0,0,0,4,2,0,2,0,1,1,0,1,0,0,0,0,0]
+s3 = [1,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,1,0,0,0,0,3,0,3,0,1]
+s4 = [0,1,0,1,0,1,0,1,0,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0]
+s5 = [0,0,0,0,0,0,0,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0]
+s6 = [4,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,3,0,0,0,4]
+tab = [s1,s2,s3,s4,s5,s6]
+output = ["","","","","",""]
+
+
+current = 0
+def convert(s,begin):
+    result = "|--"
+    for i in 5:
+        value = s[begin+i]
+        if value > 0:
+            result+= " "
+            result+= str(value - 1)
+            result+= " "
+        else:
+            result += "---"
+        result+= "----"
+    return result
+
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+
+
+while True:
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    #TODO: fill in here. You should be able to look in cli_clock.py and stats.py 
+    for i in range(6):
+        output[i] = convert(tab[i],current)
+        draw.text((0,10*i),output[i], font=font, fill = "#00FF00")
+    # Display image.
+    disp.image(image, rotation)
+    time.sleep(1)
