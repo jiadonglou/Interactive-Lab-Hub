17675943283bcd86f20324050be461f1d31a0b9b
jiadonglou <jl3937@cornell.edu>
2021-03-24 16:06:55 -0400

    Update cap_test.py

--- Lab 4/cap_test.py
+++ Lab 4/cap_test.py
@@ -24,5 +24,5 @@ while True:
 
 	for i in range(12):
 		if mpr121[i].value:
-			print(f"Banana {i} touched!")
-	time.sleep(0.25)  # Small delay to keep from spamming output messages.
+			#print(f"Banana {i} touched!")
+	#time.sleep(0.25)  # Small delay to keep from spamming output messages.
