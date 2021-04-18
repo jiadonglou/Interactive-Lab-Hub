c03c310ad8ebc2294cc91306a7eb3900ec88fccf
jiadonglou <jl3937@cornell.edu>
2021-04-18 05:09:52 -0400

    Update detect.py

--- Lab 5/openCV-examples/object-detection/detect.py
+++ Lab 5/openCV-examples/object-detection/detect.py
@@ -38,26 +38,12 @@ def detect_labels(photo, bucket):
     response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
         MaxLabels=10)
 
-    print('Detected labels for ' + photo) 
-    print()   
+    output=''
     for label in response['Labels']:
-        print ("Label: " + label['Name'])
-        print ("Confidence: " + str(label['Confidence']))
-        print ("Instances:")
-        for instance in label['Instances']:
-            print ("  Bounding box")
-            print ("    Top: " + str(instance['BoundingBox']['Top']))
-            print ("    Left: " + str(instance['BoundingBox']['Left']))
-            print ("    Width: " +  str(instance['BoundingBox']['Width']))
-            print ("    Height: " +  str(instance['BoundingBox']['Height']))
-            print ("  Confidence: " + str(instance['Confidence']))
-            print()
 
-        print ("Parents:")
-        for parent in label['Parents']:
-            print ("   " + parent['Name'])
-        print ("----------")
-        print ()
+      if label['Confidence'] > 85:
+        output=output+label['Name']+" | "
+    print("Detected:"+output, end='\r')
     return len(response['Labels'])
 
 
@@ -105,8 +91,6 @@ while(True):
           photo='detected.jpg'
           bucket='cornelltech'
           label_count=detect_labels(photo, bucket)
-          print("Labels detected: " + str(label_count))
-          print("Finished a frame")
 
           continue
         cv2.imshow('detected (press q to quit)',img)
