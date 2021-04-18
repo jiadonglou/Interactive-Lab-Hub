1b2a00de5e108bf6fb29f3f3fe7f786a8494129d
jiadonglou <jl3937@cornell.edu>
2021-04-18 04:59:30 -0400

    update

--- Lab 5/openCV-examples/object-detection/detect.py
+++ Lab 5/openCV-examples/object-detection/detect.py
@@ -5,6 +5,62 @@
 import numpy as np
 import cv2
 import sys
+import requests
+
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+
+
+def upload_to_aws(local_file, bucket, s3_file):
+
+    ACCESS_KEY = 'AKIATSIVDXPQKGK4542J'
+    SECRET_KEY = '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'
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY)
+
+    try:
+        s3.upload_file(local_file, bucket, s3_file)
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+
+
+def detect_labels(photo, bucket):
+
+    client=boto3.client('rekognition')
+
+    response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
+        MaxLabels=10)
+
+    print('Detected labels for ' + photo) 
+    print()   
+    for label in response['Labels']:
+        print ("Label: " + label['Name'])
+        print ("Confidence: " + str(label['Confidence']))
+        print ("Instances:")
+        for instance in label['Instances']:
+            print ("  Bounding box")
+            print ("    Top: " + str(instance['BoundingBox']['Top']))
+            print ("    Left: " + str(instance['BoundingBox']['Left']))
+            print ("    Width: " +  str(instance['BoundingBox']['Width']))
+            print ("    Height: " +  str(instance['BoundingBox']['Height']))
+            print ("  Confidence: " + str(instance['Confidence']))
+            print()
+
+        print ("Parents:")
+        for parent in label['Parents']:
+            print ("   " + parent['Name'])
+        print ("----------")
+        print ()
+    return len(response['Labels'])
+
+
 # Load a model imported from Tensorflow
 tensorflowNet = cv2.dnn.readNetFromTensorflow('frozen_inference_graph.pb', 'ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
 
@@ -42,23 +98,17 @@ while(True):
     # Runs a forward pass to compute the net output
     networkOutput = tensorflowNet.forward()
 
-    # Loop on the outputs
-    for detection in networkOutput[0,0]:
-        score = float(detection[2])
-        if score > 0.2:
-            left = detection[3] * cols
-            top = detection[4] * rows
-            right = detection[5] * cols
-            bottom = detection[6] * rows
-
-            #draw a red rectangle around detected objects
-            cv2.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (0, 0, 255), thickness=2)
-
     if webCam:
         if sys.argv[-1] == "noWindow":
-           print("Finished a frame")
-           cv2.imwrite('detected_out.jpg',img)
-           continue
+          cv2.imwrite('detected.jpg',img)
+          uploaded = upload_to_aws('detected.jpg', 'cornelltech', 'detected.jpg')
+          photo='detected.jpg'
+          bucket='cornelltech'
+          label_count=detect_labels(photo, bucket)
+          print("Labels detected: " + str(label_count))
+          print("Finished a frame")
+
+          continue
         cv2.imshow('detected (press q to quit)',img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cap.release()
