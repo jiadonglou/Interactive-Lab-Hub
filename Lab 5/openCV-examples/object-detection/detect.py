d9b52309d7def1afbe608229beff29b3bbded84d
jiadonglou <jl3937@cornell.edu>
2021-04-18 05:42:03 -0400

    Update detect.py

--- Lab 5/openCV-examples/object-detection/detect.py
+++ Lab 5/openCV-examples/object-detection/detect.py
@@ -88,9 +88,8 @@ while(True):
           uploaded = upload_to_aws('detected.jpg', 'cornelltech', 'detected.jpg')
           photo='detected.jpg'
           bucket='cornelltech'
-          while(not uploaded):
-            time.sleep(0.1)
           api2=detect_labels(photo, bucket)
+          time.sleep(5)
           continue
         cv2.imshow('detected (press q to quit)',img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
