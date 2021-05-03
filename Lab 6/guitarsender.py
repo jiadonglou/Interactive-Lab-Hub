4a0477ca788c657611e350757c8459066ce4d83d
jiadonglou <jl3937@cornell.edu>
2021-05-03 00:25:57 -0400

    merge

---
+++ Lab 6/guitarsender.py
@@ -0,0 +1,133 @@
+import time
+import subprocess
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+import time
+import board
+import busio
+
+import adafruit_mpr121
+import paho.mqtt.client as mqtt
+import uuid
+
+s1 = [0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+s2 = [0,0,0,0,1,0,0,0,0,0,4,0,4,0,0,0,0,0,4,2,0,2,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0]
+s3 = [0,0,0,1,0,0,1,0,1,0,0,0,0,0,0,1,0,1,0,0,1,0,1,0,1,0,1,3,0,3,0,1,0,0,0,0,0,0]
+s4 = [0,0,1,0,0,0,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0]
+s5 = [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0]
+s6 = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0]
+tab = [s1,s2,s3,s4,s5,s6]
+output = ["","","","","",""]
+
+
+current = 0
+def convert(s,begin):
+    result = "|--"
+    for i in range(5):
+        value = s[begin+i]
+        if value > 0:
+            result+= " "
+            result+= str(value - 1)
+            result+= " "
+        else:
+            result += "---"
+        result+= "----"
+    return result
+
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+mpr121 = adafruit_mpr121.MPR121(i2c)
+
+# Every client needs a random ID
+client = mqtt.Client(str(uuid.uuid1()))
+# configure network encryption etc
+client.tls_set()
+# this is the username and pw we have setup for the class
+client.username_pw_set('idd', 'device@theFarm')
+
+#connect to the broker
+client.connect(
+    'farlab.infosci.cornell.edu',
+    port=8883)
+topic = f"IDD/guitar"
+
+while True:
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    #TODO: fill in here. You should be able to look in cli_clock.py and stats.py 
+    for i in range(6):
+        output[i] = convert(tab[i],current)
+        draw.text((0,20*i),output[i], font=font, fill = "#FFFFFF")
+    # Display image.
+    disp.image(image, rotation)
+
+    correct = True
+    for j in range(6):
+        correct = correct and ((tab[j][current]>0) == (mpr121[j].value))
+    if correct:
+        current+=1
+        client.publish(topic, current)
+
+    for i in range(12):
+        if mpr121[i].value:
+            print(f"String {i} touched!")
+    time.sleep(0.1)
